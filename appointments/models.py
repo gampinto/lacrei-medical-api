e43678ee116b098adaf4cde6c96fa27582854508
Gabriel Pinto <gabriel.a.m.pinto@hotmail.com>
2026-09-15 13:08:58 -0300

    estabelecendo modelos de profissionais e sua cardinalidade

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Professional(models.Model):
+    social_name = models.CharField(max_length=150)
+    profession = models.CharField(max_length=100)
+    address = models.TextField()
+    contact = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.social_name
+
+
+class Appointment(models.Model):
+    date = models.DateTimeField()
+    professional = models.ForeignKey(
+        Professional,
+        on_delete=models.CASCADE,
+        related_name="appointments",
+    )
+
+    def __str__(self):
+        return f"{self.professional.social_name} - {self.date}"
