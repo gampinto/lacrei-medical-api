140cd29ba4eb9e42f5b4c8f9970b02a7945d1221
Gabriel Pinto <gabriel.a.m.pinto@hotmail.com>
2026-09-18 15:05:58 -0300

    adicionando pipeline de CI

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
 from django.utils import timezone
+from rest_framework import serializers
 
 from .models import Appointment, Professional
 
--- appointments/urls.py
+++ appointments/urls.py
@@ -2,7 +2,6 @@ from rest_framework.routers import DefaultRouter
 
 from .views import AppointmentViewSet, ProfessionalViewSet
 
-
 router = DefaultRouter()
 
 router.register("professionals", ProfessionalViewSet, basename="professional")
