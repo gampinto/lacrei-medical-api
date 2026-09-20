832c1acd66ac765219e652cceddffc181369e106
Gabriel Pinto <gabriel.a.m.pinto@hotmail.com>
2026-09-20 17:50:37 -0300

    ajustando CORS e removendo arquivos acidentais

--- config/settings.py
+++ config/settings.py
@@ -68,12 +68,12 @@ SPECTACULAR_SETTINGS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
